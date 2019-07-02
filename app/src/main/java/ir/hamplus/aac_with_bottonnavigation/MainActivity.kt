7847c6e8af1b5d4f870199d530c398bdcaf11861
Hamed Jaliliani <hamed.jaliliani@gmail.com>
2019-07-02 15:20:14 +0430

    -Change access to views by kotlin ways instead of findviewById -red text values from String.xml be getString

--- app/src/main/java/ir/hamplus/aac_with_bottonnavigation/MainActivity.kt
+++ app/src/main/java/ir/hamplus/aac_with_bottonnavigation/MainActivity.kt
@@ -3,23 +3,22 @@ package ir.hamplus.aac_with_bottonnavigation
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textMessage: TextView
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                textMessage.setText(R.string.title_home)
+                message.text = getString(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
-                textMessage.setText(R.string.title_dashboard)
+                message.text = getString(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
-                textMessage.setText(R.string.title_notifications)
+                message.text = getString(R.string.title_notifications )
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -29,9 +28,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        textMessage = findViewById(R.id.message)
-        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        nav_view.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
     }
 }
